7dd2c2a2320fd8b9d20eb99eb88821b552ce664e
Shuming Cui <1005586890@qq.com>
2022-06-09 16:43:30 +0800

    add comment

--- dist/index.js
+++ dist/index.js
@@ -37,6 +37,7 @@ const github = __nccwpck_require__(5438);
 const fs = __nccwpck_require__(5747);
 const path = __nccwpck_require__(5622);
 const git = __nccwpck_require__(5138);
+// 开启子进程，用于在终端执行格式检查脚本
 const { spawnSync } = __nccwpck_require__(3129);
 
 const spawnOpts = { shell: true, stdio: 'pipe', windowsHide: true };
@@ -44,6 +45,8 @@ const spawnOpts = { shell: true, stdio: 'pipe', windowsHide: true };
 function exec(cmd, args = [], opts = spawnOpts) {
   console.log('$', cmd, ...args);
   const result = spawnSync(cmd, args, opts);
+  // filter:创建包含通过所提供的函数实现的测试的所有元素的新数组
+  // => : ES6写法
   const output = result.output.filter((e) => e && e.length > 0).toString();
   console.log(output);
   if (result.status !== 0) {
@@ -52,6 +55,7 @@ function exec(cmd, args = [], opts = spawnOpts) {
   return output;
 }
 
+// ...args : 剩余参数；由没有对应形参的实参组成的一个数组
 async function gitCall(...args) {
   console.log('$ git', ...args);
   const output = await git(...args);
@@ -61,12 +65,28 @@ async function gitCall(...args) {
 
 exports.checkFormat = async function (argv) {
   console.log(argv);
+  // format : 定义在package.json中的scripts
   exec('yarn', ['run', 'format']);
   const gitStatus = await gitCall('status', '--short');
   if (gitStatus) {
     console.log('\n! found unformatted code');
+    // 字符串拼接：`words + ${字符串变量}`
     throw new Error(`Found unformatted code\n${gitStatus}`);
   }
+};
+
+exports.addPullRequestComment = async function (token, owner, repo, pullRequestNumber) {
+  const octokit = github.getOctokit(token);
+  const pullRequestQuery = await octokit.graphql(`
+    query {
+      repository(name: "${repo}", owner: "${owner}") {
+        pullRequest(number: ${pullRequestNumber}) { id }
+      }
+  }`);
+  const pullRequestID = pullRequestQuery.repository.pullRequest.id;
+  const diff = await gitCall('diff');
+  const body = `${gitStatus} + ${diff}`;
+  await octokit.graphql(`mutation{addComment(input:{subjectID:"${pullRequestID}",body:"${body}"}){subject{id}}}`);
 };
 
 
